c1bb8c6385d0568224be020f6e26f10118775ac7
sabinmhx <sabinmhx@gmail.com>
2024-08-07 14:46:45 +0545

    Refactor video controller to use reactive extensions for state management

--- lib/video_controller.dart
+++ lib/video_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_video_player_test/video_model.dart';
 import 'package:flutter_video_player_test/video_service.dart';
 import 'package:get/get.dart';
@@ -5,10 +6,11 @@ import 'package:video_player/video_player.dart';
 
 class VideoController extends GetxController {
   final VideoService videoService = VideoService();
-  var videos = <Video>[].obs;
-  var controllers = <VideoPlayerController?>[].obs;
-  var isPlaying = <bool>[].obs;
-  var currentPageIndex = 0.obs;
+  final RxList<Video> videos = RxList(<Video>[]);
+  final RxList<VideoPlayerController?> controllers =
+      RxList(<VideoPlayerController?>[]);
+  final RxList<bool> isPlaying = RxList(<bool>[]);
+  final RxInt currentPageIndex = RxInt(0);
 
   final int preloadCount = 5;
 
@@ -18,61 +20,87 @@ class VideoController extends GetxController {
     fetchVideos();
   }
 
-  void fetchVideos() async {
+  Future<void> fetchVideos() async {
     videos.value = await videoService.fetchVideos();
     controllers.value = List.generate(videos.length, (_) => null);
     isPlaying.value = List.generate(videos.length, (_) => false);
-    preloadVideos(currentPageIndex.value);
+    if (videos.isNotEmpty) {
+      await preloadVideos(currentPageIndex.value);
+    }
   }
 
-  void preloadVideos(int centerIndex) {
+  Future<void> preloadVideos(int centerIndex) async {
     final start = (centerIndex - preloadCount).clamp(0, videos.length - 1);
     final end = (centerIndex + preloadCount).clamp(0, videos.length - 1);
 
     for (int i = start; i <= end; i++) {
-      if (controllers[i] == null) {
-        controllers[i] =
-            VideoPlayerController.networkUrl(Uri.parse(videos[i].url))
-              ..initialize().then((_) {
-                // Play the video if it's the current page
-                if (i == currentPageIndex.value) {
-                  playVideo(i);
-                }
-                update(); // Update the state
-              });
-      }
+      await _initializeController(i);
     }
 
+    _disposeUnneededControllers(start, end);
+    update;
+  }
+
+  Future<void> _initializeController(int index) async {
+    if (controllers[index] == null) {
+      final controller =
+          VideoPlayerController.networkUrl(Uri.parse(videos[index].url));
+      controllers[index] = controller;
+      try {
+        // Add a listener to update state when the video is initialized
+        controller.addListener(() {
+          if (controller.value.isInitialized) {
+            debugPrint('Video at index $index initialized');
+            if (index == currentPageIndex.value) {
+              playVideo(index);
+            }
+          }
+        });
+
+        // Initialize the controller
+        await controller.initialize();
+      } catch (e) {
+        debugPrint('Error initializing video at index $index: $e');
+        controllers[index] = null;
+      }
+    }
+  }
+
+  void _disposeUnneededControllers(int start, int end) {
     for (int i = 0; i < controllers.length; i++) {
       if (i < start || i > end) {
         controllers[i]?.dispose();
         controllers[i] = null;
-        isPlaying[i] = false; // Ensure state is updated
+        isPlaying[i] = false;
       }
     }
   }
 
-  void playVideo(int index) {
-    if (controllers[index] != null && !isPlaying[index]) {
-      controllers[index]!.play();
-      isPlaying[index] = true;
-      update();
+  Future<void> playVideo(int index) async {
+    if (controllers[index] != null) {
+      try {
+        await controllers[index]!.play();
+        isPlaying[index] = true;
+        update;
+      } catch (e) {
+        debugPrint('Error playing video at index $index: $e');
+      }
     }
   }
 
-  void pauseVideo(int index) {
+  Future<void> pauseVideo(int index) async {
     if (controllers[index] != null && isPlaying[index]) {
-      controllers[index]!.pause();
+      await controllers[index]!.pause();
       isPlaying[index] = false;
-      update();
+      update;
     }
   }
 
-  void onPageChanged(int index) {
-    pauseVideo(currentPageIndex.value);
+  Future<void> onPageChanged(int index) async {
+    await pauseVideo(currentPageIndex.value);
     currentPageIndex.value = index;
-    playVideo(index);
-    preloadVideos(index);
+    await playVideo(index);
+    await preloadVideos(index);
   }
 
   @override

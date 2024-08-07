f6c7aa7c1f8f5f34bd63ccd73b3adf7b98cc8947
sabinmhx <sabinmhx@gmail.com>
2024-08-07 11:25:24 +0545

    chore: Add video feed and video item components

---
+++ lib/video_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoItem extends StatefulWidget {
+  final String videoUrl;
+  final bool isPlaying;
+
+  const VideoItem({super.key, required this.videoUrl, required this.isPlaying});
+
+  @override
+  State<VideoItem> createState() => _VideoItemState();
+}
+
+class _VideoItemState extends State<VideoItem> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // ignore: deprecated_member_use
+    _controller = VideoPlayerController.network(widget.videoUrl)
+      ..initialize().then((_) {
+        if (widget.isPlaying) {
+          _controller.play();
+        }
+        setState(() {});
+      });
+  }
+
+  @override
+  void didUpdateWidget(VideoItem oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isPlaying != oldWidget.isPlaying) {
+      widget.isPlaying ? _controller.play() : _controller.pause();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          )
+        : const Center(child: CircularProgressIndicator());
+  }
+}
